f27c5e97db8c6f6a2b872dbff1ff3e1f7fd8e21d
RicTieri <tieri.riccardo@gmail.com>
2023-12-18 17:46:02 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <AppHeader/>
+  <AppMain/>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
+export default {
+    components: { AppHeader, AppMain }
+}
+</script>
+
+<style lang="scss">
+  
+</style>

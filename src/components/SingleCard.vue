b18b220736d471b72d0647b14cf4f69e6cae0d0a
RicTieri <tieri.riccardo@gmail.com>
2023-12-19 15:33:18 +0100

    added select

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -2,7 +2,6 @@
   <div class="card m-3 p-0" style="width: 18rem;">
   <img :src="card.card_images[0].image_url" class="card-img-top" :alt="card.name">
   <div class="card-body">
-    a
     <h5 class="card-title">{{ card.name }}</h5>
   </div>
 </div>
